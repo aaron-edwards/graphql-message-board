559c5fc5448ea64a57f38ff44a6377f0f756e45c
aaron-edwards <edwards.aaronmark@gmail.com>
2019-09-23 18:42:44 +1000

    Added fields for quotes

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,12 @@
+const quote = {
+  id: 1,
+  text: 'Some Text',
+  userId: 1,
+  category: 'LL',
+};
+
 export default {
   Query: {
-    quotes: () => [],
+    quotes: () => [quote],
   },
 };
--- src/type-defs.ts
+++ src/type-defs.ts
@@ -1,8 +1,14 @@
 import { gql } from 'apollo-server';
 
 export default gql`
+  enum Category {
+    LL
+  }
   type Quote {
+    id: Int!
     text: String!
+    userId: Int!
+    category: Category!
   }
 
   type Query {
