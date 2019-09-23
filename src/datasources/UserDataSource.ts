ae1f288568a25d2eafc5d2ea12801f371497b05e
Aaron Edwards <edwards.aaronmark@gmail.com>
2019-09-23 20:21:47 +1000

    Added a context function to check Auth Headers

--- src/datasources/UserDataSource.ts
+++ src/datasources/UserDataSource.ts
@@ -1,14 +1,19 @@
-import { RESTDataSource, Response, Request } from 'apollo-datasource-rest';
+import { RESTDataSource, Response, Request, RequestOptions } from 'apollo-datasource-rest';
 
 export default class UserDataSource extends RESTDataSource {
   constructor() {
     super();
     this.baseURL = "https://jsonplaceholder.typicode.com"
   }
+
+  protected willSendRequest?(request: RequestOptions) {
+    request.headers.set("Authorization", this.context.token);
+  }
+
   getUser(id: number) {
     return this.get(`/users/${id}`)
   }
-  
+
   protected async didReceiveResponse(response: Response, request: Request) {
     console.log(`Response From ${response.url} ${response.status}`);
     if (response.status === 404) {
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer, AuthenticationError } from 'apollo-server';
 import typeDefs from './type-defs';
 import resolvers from './resolvers';
 import dataSources from './datasources';
 
 const port = process.env.PORT || 4000;
 
+const context = ({ req }: { req: any }) => {
+  const token = req.headers.authorization;
+
+  if (token !== "Token") {
+    throw new AuthenticationError("Invalid Token Found");
+  }
+
+  return { token };
+}
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources,
+  context,
   introspection: true,
   playground: true,
 });
